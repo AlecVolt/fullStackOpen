1520e61cee4f8ac6f19fa0fd837bddaabd69fa37
Alesya Volota <foxavolt@gmail.com>
2025-09-02 15:05:13 +0200

    done ex 1.5

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,6 +1,6 @@
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.name}</h1>
   )
 }
 
@@ -33,34 +33,31 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ];
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}/>
+      <Header name={course.name}/>
 
-      <Content 
-        parts={parts}
-      />
+      <Content parts={course.parts} />
 
-      <Total parts={parts} />
-
-      {/* <Total number={part1.exercises + part2.exercises + part3.exercises} /> */}
+      <Total parts={course.parts} />
     </div>
   )
 
