62840fca9a111fce415e5cb8c6cb8d8065f51e5b
Alesya Volota <foxavolt@gmail.com>
2025-09-09 16:26:13 +0200

    done ex 2.20

--- part2/countries/src/SingleCountry.jsx
+++ part2/countries/src/SingleCountry.jsx
@@ -1,15 +1,24 @@
 import { useEffect, useState } from "react";
 import BackToMainBtn from "./BackToMainBtn";
 import { getCountyByName } from "./services/countries";
+import { getWeather } from "./services/weather";
 
 const SingleCountry = ({ name, setShownCountryName }) => {
     const [ countryInfo, setCountyInfo ] = useState(null);
+    const [ weatherInfo, setWeatherInfo ] = useState(null);
 
     useEffect(() => {
         getCountyByName(name)
             .then(data => setCountyInfo(data));
     }, []);
 
+    useEffect(() => {
+        console.log('countryInfo');
+        if (countryInfo) {
+            getWeather(countryInfo.capital[0])
+            .then(data => setWeatherInfo(data));
+        }
+    }, [countryInfo]);    
 
     return (
         <>
@@ -44,6 +53,16 @@ const SingleCountry = ({ name, setShownCountryName }) => {
                     <div>
                         <img src={countryInfo.flags.png} alt={countryInfo.flags.alt} />
                     </div>
+
+                    <h3>Weather in {countryInfo.capital[0]}</h3>
+                    {
+                        weatherInfo &&
+                        <div>
+                            <p>Temperature {weatherInfo.main.temp} Celsius</p>
+                            <img src={`https://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@2x.png`} />
+                            <p>Wind {weatherInfo.wind.speed} m/s</p>
+                        </div>
+                    }
                 </div>
             }
         </>
---
+++ part2/countries/src/services/weather.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const weatherApiKey = import.meta.env.VITE_WEATHER_KEY;
+console.log(weatherApiKey);
+
+export const getWeather = (name) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&appid=${weatherApiKey}`;
+    
+    return axios
+        .get(url)
+        .then(response => response.data);
+}
