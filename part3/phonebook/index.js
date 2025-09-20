395706e99a29e45cf1728117432c3b7bd71491b9
Alesya Volota <foxavolt@gmail.com>
2025-09-20 18:54:00 +0200

    done ex 3.5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-
+app.use(express.json());
 
 let persons = [
     { 
@@ -56,6 +56,30 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id);
 
     response.status(204).end();
+});
+
+const generateId = () => {
+    return String(Math.floor(Math.random() * 10000));
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+
+    if (!body) {
+        return response.status(400).json(
+            { error: 'Content missing' }
+        );
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons.concat(person);
+
+    response.json(person);
 });
 
 const PORT = 3001;
