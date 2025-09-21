40017d84809d2fdfaf070a051887fd152844de8b
Alesya Volota <foxavolt@gmail.com>
2025-09-21 17:16:44 +0200

    done ex 3.10

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.static('dist'))
 
 const morgan = require('morgan');
 
@@ -115,7 +116,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person);
 });
 
-const PORT = 3001; 
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
