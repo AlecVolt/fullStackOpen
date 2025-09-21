d1e4ef62fdce78cab6a65c9e009a7e445a9e2b37
Alesya Volota <foxavolt@gmail.com>
2025-09-21 14:49:12 +0200

    deploying app

--- part3/notesBackend/index.js
+++ part3/notesBackend/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.static('dist'));
+
 let notes = [
   {
     id: "1",
@@ -75,7 +80,7 @@ app.post('/api/notes', (request, response) => {
     response.json(note);
 });
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
