6ba23b1cf1b748364cd305b6451b5accd889fa86
Alesya Volota <foxavolt@gmail.com>
2025-09-23 14:15:14 +0200

    done ex 3.15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -25,10 +25,16 @@ app.get('/api/persons', (request, response) => {
     });
 });
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person);
-    });
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (note) {
+                response.json(person);
+            } else {
+                response.status(404).end();
+            }
+        })
+        .catch(error => next(error));
 });
 
 app.post('/api/persons', (request, response) => {
@@ -62,11 +68,31 @@ app.post('/api/persons', (request, response) => {
     })
 });
 
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+        response.status(204).end();
+    })
+    .catch(error => next(error));
+});
+
 app.get('/info', (request, response) => {
     Person.find({}).then(persons => {
         response.send(`Phonebook has info for ${persons.length}</br> ${new Date()}`);
     });
 });
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformed id' });
+    }
+
+    next(error);
+}
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
