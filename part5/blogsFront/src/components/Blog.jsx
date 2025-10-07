97cfde104912da8cc8c820f5285e1f890a85dbc1
Alesya Volota <foxavolt@gmail.com>
2025-10-07 18:53:11 +0200

    done ex 5.7

--- part5/blogsFront/src/components/Blog.jsx
+++ part5/blogsFront/src/components/Blog.jsx
@@ -1,7 +1,27 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+import './blog.css'
+const Blog = ({ blog }) => {
+  const [isView, setIsView] = useState(false)
+  const buttonLabel = isView ? 'hide' : 'view'
+  
+  const toggleIsView = () => {
+    setIsView(prev => !prev)
+  }
+
+  return (
+    <div className='blogItem'>
+      <p>
+        "{blog.title}" by {blog.author}
+        <button className='button' onClick={toggleIsView}>{buttonLabel}</button>
+      </p> 
+      
+      {isView && <>
+        <p>{blog.url}</p>
+        <p>likes {blog.likes}</p>
+        <p>{blog.user?.name}</p>
+      </>}
+    </div>  
+  )
+}
 
 export default Blog
