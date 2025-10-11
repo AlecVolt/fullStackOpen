4db959e28cccf9864b68a525d80ec27dc14e91ed
Alesya Volota <foxavolt@gmail.com>
2025-10-11 13:48:53 +0200

    done ex 5.14

--- part5/blogsFront/src/components/Blog.test.jsx
+++ part5/blogsFront/src/components/Blog.test.jsx
@@ -1,13 +1,14 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
-  test('renders the blogs title and author', () => {
+  beforeEach(() => {
     const blog = {
       title: 'test title',
       author: 'test author',
       url: 'test url',
-      likes: 'test likes',
+      likes: 5,
       user: {
         name: 'test user name',
         username: 'test user username'
@@ -15,7 +16,9 @@ describe('<Blog />', () => {
     }
 
     render(<Blog blog={blog} />)
+  })
 
+  test('renders the blogs title and author', () => {
     const titleElement = screen.getByText('test title')
     expect(titleElement).toBeVisible()
 
@@ -28,28 +31,34 @@ describe('<Blog />', () => {
     const urlElement = screen.queryByText('test url')
     expect(urlElement).toBeNull()
 
-    const likesElement = screen.queryByText('test likes')
+    const likesElement = screen.queryByText('5')
     expect(likesElement).toBeNull()
 
     const userElement = screen.queryByText('test user name')
     expect(userElement).toBeNull()
   })
 
-  // test('renders the blogs title and author', () => {
-  //   const blog = {
-  //     title: 'test title',
-  //     author: 'test author',
-  //     url: 'test url',
-  //     likes: 'test likes',
-  //     // user: {
-  //     //   name: 'test user name',
-  //     //   username: 'test user username'
-  //     // }
-  //   }
+  test('after clicking the button, other info are displayed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
 
-  //   const { container } = render(<Blog blog={blog} />)
+    const titleElement = screen.getByText('test title')
+    expect(titleElement).toBeVisible()
 
-  //   const element = screen.getByText('test title')
-  //   expect(element).toBeDefined()
-  // })
+    const authorElement = screen.getByText('test author')
+    expect(authorElement).toBeVisible()
+
+    const viewButtonElement = screen.getByText('hide')
+    expect(viewButtonElement).toBeVisible()
+
+    const urlElement = screen.getByText('test url')
+    expect(urlElement).toBeVisible()
+
+    const likesElement = screen.getByText('5', { exact: false })
+    expect(likesElement).toBeVisible()
+
+    const userElement = screen.getByText('test user name')
+    expect(userElement).toBeVisible()
+  })
 })
