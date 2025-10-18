d6e260c00efaba93a444ae7b6caed86a1d7e6366
Alesya Volota <foxavolt@gmail.com>
2025-10-18 14:05:11 +0200

    done ex 6.15

--- part6/anecdotes/src/components/AddAnecdoteForm.jsx
+++ part6/anecdotes/src/components/AddAnecdoteForm.jsx
@@ -2,16 +2,20 @@ import { useDispatch } from "react-redux"
 import { createAnecdote } from "../reducers/anecdoteReducer"
 import { setNotification } from "../reducers/notificationReducer"
 
+import anecdotesService from '../services/anecdotes'
+
 const AddAnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
 
     const text = event.target.anecdoteText.value
     event.target.anecdoteText.value = ''
+
+    const newText = await anecdotesService.createNew(text)
     
-    dispatch(createAnecdote(text))
+    dispatch(createAnecdote(newText))
     dispatch(setNotification(`You added '${text}'`))
   }
 
--- part6/anecdotes/src/reducers/anecdoteReducer.js
+++ part6/anecdotes/src/reducers/anecdoteReducer.js
@@ -1,38 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit"
-
-// const initialAnecdotes = [
-//   {
-//     id: 1,
-//     text: 'If it hurts, do it more often.',
-//     votes: 1
-//   },
-//   {
-//     id: 2,
-//     text: 'Adding manpower to a late software project makes it later!',
-//     votes: 0
-//   },
-//   {
-//     id: 3,
-//     text: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-//     votes: 11
-//   },
-//   {
-//     id: 4,
-//     text: 'Premature optimization is the root of all evil.',
-//     votes: 5
-//   },
-//   {
-//     id: 5,
-//     text: 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-//     votes: 5
-//   },
-// ]
-
 const sortAnecdotes = (arr) => {
   return arr.sort((a, b) => b.votes - a.votes)
 }
-
-const generateId = () => Number(Math.random() * 100000)
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
@@ -42,15 +11,7 @@ const anecdoteSlice = createSlice({
       return sortAnecdotes(action.payload)
     },
     createAnecdote(state, action) {
-      const text = action.payload
-
-      return sortAnecdotes([
-        ...state, {
-          text,
-          votes: 0,
-          id: generateId()
-        }
-      ])
+      return sortAnecdotes([...state, action.payload])
     },
     addVote(state, action) {
       const id = action.payload
