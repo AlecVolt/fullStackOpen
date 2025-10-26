cbb5c2785e32d5aa548e35d871ddf23d9454c4e0
Alesya Volota <foxavolt@gmail.com>
2025-10-26 14:41:21 +0100

    done ex 8.7

--- part8/libraryBack/index.js
+++ part8/libraryBack/index.js
@@ -111,6 +111,11 @@ const typeDefs = `
       author: String!
       genres: [String!]!
     ) : Book
+
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ) : Author
   }
 `
 
@@ -149,6 +154,18 @@ const resolvers = {
       }
 
       return book
+    },
+
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name)
+
+      if (!author) {
+        return null
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map((a) => (a.name !== args.name ? a : updatedAuthor))
+      return updatedAuthor
     },
   },
 
