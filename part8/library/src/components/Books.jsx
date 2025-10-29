fc0595576808f566380d50b29cea931202f3ae85
Alesya Volota <foxavolt@gmail.com>
2025-10-29 17:42:26 +0100

    done ex 8.19

--- part8/library/src/components/Books.jsx
+++ part8/library/src/components/Books.jsx
@@ -1,31 +1,72 @@
 import { useQuery } from '@apollo/client/react'
 import { ALL_BOOKS } from '../queries/books'
+import { useState } from 'react'
 
 const Books = () => {
   const result = useQuery(ALL_BOOKS)
+  const [genre, setGenre] = useState('all')
 
   if (result.loading) {
     return <div>loading...</div>
+  }
+
+  const genres = Array.from(
+    new Set(
+      result.data.allBooks
+        .map((b) => b.genres.join(','))
+        .join(',')
+        .split(',')
+    )
+  )
+
+  const filterBooks = ({ target }) => {
+    setGenre(target.value)
   }
 
   return (
     <div>
       <h2>books</h2>
 
-      <table>
+      <div>
+        <p>
+          in genre <b>{genre}</b>
+        </p>
+        <select value={genre} onChange={filterBooks}>
+          <option key={'all'} value="all">
+            all
+          </option>
+          {genres.map((g) => (
+            <option key={g} value={g}>
+              {g}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <table style={{ width: '100%' }}>
         <tbody>
           <tr>
-            <th></th>
-            <th>author</th>
-            <th>published</th>
+            <th style={{ width: '60%' }}>title</th>
+            <th style={{ width: '20%' }}>author</th>
+            <th style={{ width: '10%' }}>published</th>
           </tr>
-          {result.data.allBooks.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {genre === 'all'
+            ? result.data.allBooks.map((b) => (
+                <tr key={b.title}>
+                  <td>{b.title}</td>
+                  <td>{b.author.name}</td>
+                  <td>{b.published}</td>
+                </tr>
+              ))
+            : result.data.allBooks
+                .filter((b) => b.genres.includes(genre))
+                .map((b) => (
+                  <tr key={b.title}>
+                    <td>{b.title}</td>
+                    <td>{b.author.name}</td>
+                    <td>{b.published}</td>
+                  </tr>
+                ))}
         </tbody>
       </table>
     </div>
