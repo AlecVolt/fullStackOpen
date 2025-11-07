9eb44345a47e583e2875e0171c8a9a04b8bcf10a
Alesya Volota <foxavolt@gmail.com>
2025-11-07 16:42:24 +0100

    done ex 9.10

--- part9/patientor/src/index.ts
+++ part9/patientor/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosisRouter from './routes/diagnoses';
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -7,6 +9,8 @@ app.use(express.json());
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosisRouter);
 
 const PORT = 3001;
 app.listen(PORT, () => {
